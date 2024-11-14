96279e67af6feb8228229f2e197caa8a5d7d45eb
Marquis <abahmarquis@gmail.com>
2024-11-14 11:50:17 -0800

    integrated endpoint to assign maintenance to service providers

--- api_factory/modules/maintenance.ts
+++ api_factory/modules/maintenance.ts
@@ -1,7 +1,7 @@
 import { GATEWAY_ENDPOINT } from '../axios.config'
 export const maintenance_api = {
     $_assign_maintenance_to_service_provider: (reqId: string, spid: string) => {
-        let url = `/maintenance-requests/${reqId}/service-providers/${spid}`
+        let url = `/maintenance-requests/${reqId}/service-providers/${spid}/assign`
         return GATEWAY_ENDPOINT.patch(url);
       },
       $_fetch_maitenence_request: (page = 1, perPage = 20, status = 'pending', tenantId = '', houseId = '') => {

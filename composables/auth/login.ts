89b142d08ffe78b288eb960ed2c890984008c51e
Marquis <abahmarquis@gmail.com>
2025-02-26 17:09:10 +0100

    upated

--- composables/auth/login.ts
+++ composables/auth/login.ts
@@ -28,7 +28,6 @@ export const use_auth_login = () => {
         password: credential.password.value,
         app: 'admin-app'
       });
-      // window.location.href = "/dashboard"
 
       if(res.status == 200 || res.status == 201){
         console.log(res, 'res here');
@@ -40,7 +39,6 @@ export const use_auth_login = () => {
           duration: 3000
         });
         router.push("/dashboard");
-        // window.location.href = "/dashboard"
       }
 
     } catch (error) {

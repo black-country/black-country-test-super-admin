39860b35a5bc4b29a4e35f6366b97d0bf27d6a6c
TireniA <tirenialabi@gmail.com>
2025-03-21 06:30:52 +0100

    bug-fixes

--- components/Property/PaymentHistory.vue
+++ components/Property/PaymentHistory.vue
@@ -2,16 +2,12 @@
   <main class="">
 
     <div class="border-[0.5px] border-gray-100 rounded-lg">
-      <div
-        class="bg-white border-[0.5px] border-gray-50 rounded-lg overflow-hidden"
-      >
+      <div class="bg-white border-[0.5px] border-gray-50 rounded-lg overflow-hidden">
         <div class="custom-scrollbar-container w-full">
           <table class="min-w-full bg-white w-full">
             <thead class="border-b-[0.5px] border-gray-50 z-30 bg-gray-25 sticky top-0">
               <tr>
-                <th
-                  class="py-5 px-5 text-left text-sm font-medium text-gray-500 tracking-wider"
-                >
+                <th class="py-5 px-5 text-left text-sm font-medium text-gray-500 tracking-wider">
                   Tenant Name
                 </th>
                 <th class="py-5 px-5 text-left text-sm font-medium text-gray-500 tracking-wider">
@@ -34,55 +30,55 @@
                 </th>
               </tr>
             </thead>
-            <tbody  v-if="!loadingPaymentHistory" class="divide-y divide-gray-50 z-10">
-              <tr
-                class="cursor-pointer"
-                v-for="(item, index) in paymentList"
-                :key="item.id"
-              >
-                <td  class="py-5 px-5 whitespace-nowrap text-left text-sm text-[#667185] font-semibold relative">
-                      <p>
-                      {{ item?.tenant?.firstName }} {{ item?.tenant?.lastName }}
-                      </p>
-                  </td>
-                  <td  class="py-5 px-5 whitespace-nowrap text-left text-sm text-[#667185] font-semibold relative">
-                      <p>
-                      {{ item?.house?.name ?? 'Nil' }} 
-                      </p>
-                  </td>
-                  <td  class="py-5 px-5 whitespace-nowrap text-left text-sm text-[#667185] font-semibold relative">
-                      <p>
-                      {{ item?.room?.name ?? 'Nil' }} 
-                      </p>
-                  </td>
-                  <td  class="py-5 px-5 whitespace-nowrap text-left text-sm text-[#667185] font-semibold relative">
-                      <p>
-                      {{ item?.rentFrequency ?? 'Nil' }} 
-                      </p>
-                  </td>
-                  <td  class="py-5 px-5 whitespace-nowrap text-left text-sm text-[#667185] font-semibold relative">
-                      <p>
-                      {{ item?.room?.paymentCount ?? 'Nil' }} 
-                      </p>
-                  </td>
-                  <!-- <td  class="py-5 px-5 whitespace-nowrap text-left text-sm text-[#667185] font-semibold relative">
-                      <p>
-                      {{ item?.status ?? 'Nil' }} 
-                      </p>
-                  </td> -->
-                  <td  class="py-5 px-5 whitespace-nowrap text-left text-sm text-[#667185] font-semibold relative">
-                        <p>
-                        <span class="rounded-full px-3 py-2 text-sm font-semibold" :class="[item.tenant.status === 'not paid' ? 'text-[#BA110B] bg-[#FBEAE9]' : 'text-[#039855] bg-[#E7F6EC]']">
-                          {{ item?.tenant?.status.toUpperCase() ?? 'Nil' }} 
-                        </span>
-                        </p>
-                    </td>
-                  <td  class="py-5 px-5 whitespace-nowrap text-left text-sm text-[#667185] font-semibold relative">
-                      <p>
-                      <!-- {{ item?.dueDate ?? 'Nil' }}  -->
-                      {{ moment(item?.dueDate).format('DD MMMM YYYY') ?? 'Nil' }}
-                      </p>
-                  </td>
+            <tbody v-if="!loadingPaymentHistory" class="divide-y divide-gray-50 z-10">
+              <tr class="cursor-pointer" v-for="(item, index) in paymentList" :key="item.id">
+                <td class="py-5 px-5 whitespace-nowrap text-left text-sm text-[#667185] font-semibold relative">
+                  <p>
+                    {{ item?.tenant?.firstName }} {{ item?.tenant?.lastName }}
+                  </p>
+                </td>
+                <td class="py-5 px-5 whitespace-nowrap text-left text-sm text-[#667185] font-semibold relative">
+                  <p>
+                    {{ item?.house?.name ?? 'Nil' }}
+                  </p>
+                </td>
+                <td class="py-5 px-5 whitespace-nowrap text-left text-sm text-[#667185] font-semibold relative">
+                  <p>
+                    {{ item?.room?.name ?? 'Nil' }}
+                  </p>
+                </td>
+                <td class="py-5 px-5 whitespace-nowrap text-left text-sm text-[#667185] font-semibold relative">
+                  <p>
+                    {{ item?.rentFrequency ?? 'Nil' }}
+                  </p>
+                </td>
+                <td class="py-5 px-5 whitespace-nowrap text-left text-sm text-[#667185] font-semibold relative">
+                  <p>
+                    {{ item?.paymentsMade ?? 'Nil' }}
+                  </p>
+                </td>
+                <!-- <td class="py-5 px-5 whitespace-nowrap text-left text-sm text-[#667185] font-semibold relative">
+                  <p>
+                    <span class="rounded-full px-3 py-2 text-sm font-semibold"
+                      :class="[item.tenant.status === 'not paid' ? 'text-[#BA110B] bg-[#FBEAE9]' : 'text-[#039855] bg-[#E7F6EC]']">
+                      {{ item?.tenant?.status.toUpperCase() ?? 'Nil' }}
+                    </span>
+                  </p>
+                </td> -->
+                <td class="py-5 px-5 whitespace-nowrap text-left text-sm text-[#667185] font-semibold relative">
+                  <p>
+                    <span class="rounded-full px-3 py-2 text-sm font-semibold"
+                      :class="[item.currentRentStatus=== 'not paid' ? 'text-[#BA110B] bg-[#FBEAE9]' : 'text-[#039855] bg-[#E7F6EC]']">
+                      {{ item?.currentRentStatus.toUpperCase() ?? 'Nil' }}
+                    </span>
+                  </p>
+                </td>
+                <td class="py-5 px-5 whitespace-nowrap text-left text-sm text-[#667185] font-semibold relative">
+                  <p>
+                    <!-- {{ item?.dueDate ?? 'Nil' }}  -->
+                    {{ moment(item?.dueDate).format('DD MMMM YYYY') ?? 'Nil' }}
+                  </p>
+                </td>
               </tr>
             </tbody>
           </table>
@@ -94,44 +90,25 @@
             </div>
           </section>
         </div>
-        <CorePagination
-          v-if="!loadingPaymentHistory && paymentList.length > 0"
-          :total="metadata.total"
-          :page="metadata.page"
-          :perPage="metadata.perPage"
-          :pages="metadata.pages"
-          @page-changed="handlePageChange"
-        />
+        <CorePagination v-if="!loadingPaymentHistory && paymentList.length > 0" :total="metadata.total"
+          :page="metadata.page" :perPage="metadata.perPage" :pages="metadata.pages" @page-changed="handlePageChange" />
       </div>
-      <div
-        v-if="!loadingPaymentHistory && paymentList.length === 0"
-        class="flex justify-center items-center flex-col my-20"
-      >
+      <div v-if="!loadingPaymentHistory && paymentList.length === 0"
+        class="flex justify-center items-center flex-col my-20">
         <div class="flex justify-center flex-col items-center gap-y-4 items-center">
           <img src="@/assets/icons/activities-empty-state.svg" />
           <p class="font-medium text-gray-400">No payment history found.</p>
         </div>
       </div>
     </div>
-    
 
-    <TenantConfigTableModal
-      v-if="tenantConfigModal"
-      @close="tenantConfigModal = false"
-    />
-    <TenantFilterModal
-      v-if="tenantFilterModal"
-      @close="tenantFilterModal = false"
-      @applyFilters="handleApplyFilters"
-    />
+
+    <TenantConfigTableModal v-if="tenantConfigModal" @close="tenantConfigModal = false" />
+    <TenantFilterModal v-if="tenantFilterModal" @close="tenantFilterModal = false" @applyFilters="handleApplyFilters" />
 
     <CoreModal :showCloseBtn="true" title="Configure Table" :isOpen="showModal" @close="showModal = false">
       <div>
-        <div
-          v-for="(column, index) in columns"
-          :key="index"
-          class="flex items-center mb-2"
-        >
+        <div v-for="(column, index) in columns" :key="index" class="flex items-center mb-2">
           <span class="flex-1 text-sm text-[#1D2739]">{{ column.label }}</span>
           <!-- <label :for="column.label" class="inline-flex items-center space-x-4 cursor-pointer dark:text-gray-100">
             <span class="relative">
@@ -143,23 +120,22 @@
           <label :for="column.label" class="inline-flex items-center space-x-4 cursor-pointer dark:text-gray-100">
             <span class="relative">
               <input :id="column.label" type="checkbox" v-model="column.visible" class="hidden peer toggle">
-              <div class="w-10 h-6 rounded-full shadow-inner bg-gray-300 dark:bg-[#F0F2F5] peer-checked:bg-green-500 peer-checked:dark:bg-[#099137]">
+              <div
+                class="w-10 h-6 rounded-full shadow-inner bg-gray-300 dark:bg-[#F0F2F5] peer-checked:bg-green-500 peer-checked:dark:bg-[#099137]">
               </div>
-              <div class="absolute inset-y-0 left-0 w-4 h-4 m-1 rounded-full shadow bg-white peer-checked:right-0 peer-checked:left-auto dark:bg-[#FFFFFF]"></div>
+              <div
+                class="absolute inset-y-0 left-0 w-4 h-4 m-1 rounded-full shadow bg-white peer-checked:right-0 peer-checked:left-auto dark:bg-[#FFFFFF]">
+              </div>
             </span>
           </label>
         </div>
         <div class="flex justify-between mt-10 gap-x-6">
-          <button
-            @click="resetColumns"
-            class="bg-[#EBE5E0] w-full font-medium text-[#292929] text-sm px-4 py-2.5 rounded-lg"
-          >
+          <button @click="resetColumns"
+            class="bg-[#EBE5E0] w-full font-medium text-[#292929] text-sm px-4 py-2.5 rounded-lg">
             Reset
           </button>
-          <button
-            @click="saveColumns"
-            class="bg-[#292929] text-sm font-medium w-full text-white px-4 py-2.5 rounded-lg"
-          >
+          <button @click="saveColumns"
+            class="bg-[#292929] text-sm font-medium w-full text-white px-4 py-2.5 rounded-lg">
             Save
           </button>
         </div>
@@ -169,7 +145,7 @@
 </template>
 
 <script lang="ts" setup>
-          import moment from "moment";
+import moment from "moment";
 import { useGetPropertyPaymentHistory } from '@/composables/modules/property/useFetchPropertyPaymentHistories'
 import { useRouter, useRoute } from "vue-router";
 const { paymentList, loadingPaymentHistory, fetchPaymentHistory, metadata, setPaginationObj, filters } = useGetPropertyPaymentHistory()
@@ -199,7 +175,7 @@ onMounted(() => {
     activeTab.value = route.query.activeTab;
   }
 });
-    
+
 </script>
 
 <style scoped>
@@ -241,8 +217,10 @@ onMounted(() => {
   max-height: 700px;
   overflow-y: auto;
   overflow-x: auto;
-  scrollbar-width: thin; /* For Firefox */
-  scrollbar-color: #5B8469 #F1F1F1; /* For Firefox */
+  scrollbar-width: thin;
+  /* For Firefox */
+  scrollbar-color: #5B8469 #F1F1F1;
+  /* For Firefox */
 }
 
 .custom-scrollbar-container::-webkit-scrollbar {
@@ -260,7 +238,7 @@ onMounted(() => {
 }
 
 .custom-scrollbar-container:hover::-webkit-scrollbar-thumb {
-  background-color: #5B8469; /* Change on hover for better visibility */
+  background-color: #5B8469;
+  /* Change on hover for better visibility */
 }
-
 </style>
